2bd1fa1521f4c2f951d33c5713b8105e1052c9e9
yth011026 <yth011026@gmail.com>
2020-10-15 01:08:44 +0900

    Create 1이 될 때까지.py

---
+++ Greedy/1이 될 때까지.py
@@ -0,0 +1,33 @@
+'''
+n, k = map(int, input().split())
+count = 0
+while n != 1:
+    if n % k == 0:
+        n //= k
+        count += 1
+    else:
+        n -= 1
+        count += 1
+print(count)
+'''
+
+# N, K공백을 기준으로 구분하여 입력 받기
+n, k = map(int, input().split())
+
+result = 0
+
+while True:
+    # N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
+    # K로 나누기
+    result += 1
+    n //= k
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n - 1)
+print(result)
