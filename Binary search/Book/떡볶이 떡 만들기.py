dd0d3153b95b3e75b161c2616e4b61e8c83212b0
yth011026 <yth011026@gmail.com>
2020-10-26 17:51:08 +0900

    Create 떡볶이 떡 만들기.py

---
+++ Binary search/Book/떡볶이 떡 만들기.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+
+start = 0
+end = max(array)
+
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        if x > mid:
+            total += x - mid
+    # 떡볶이 양이 부족한 경우 더 많이 자르기 (오른쪽 부분 탐색)
+    if total < m:
+        end = mid - 1
+    # 떡볶이 양이 충분한 경우 덜 자르기 (왼쪽 부분 탐색)
+    else:
+        result = mid # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
+        start = mid + 1
+
+print(result)
